5c0e2310e7bf8ca470af99efcc7b2f3bde706164
Mateo Zouko <mateozouko1@gmail.com>
2024-11-07 18:40:17 -0300

    creating the database context

---
+++ Data/DbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Models;
+
+namespace Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Users> Users { get; set; }
+    }
+}
